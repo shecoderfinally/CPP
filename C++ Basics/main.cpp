c129e81b796f3df9052ae1df7aff8ded98aa8e11
shecoderfinally <53052899+shecoderfinally@users.noreply.github.com>
2022-06-21 20:03:12 +0530

    Add files via upload

---
+++ C++ Basics/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade='S';
+    string phrase="Giraffe Academy";
+    int age=50;
+    float gpa=2.3;
+    double cgpa=2.0;
+    bool isMale=true;
+
+    cout<< grade <<endl;
+    cout<< phrase <<endl;
+    cout<< age <<endl;
+    cout<< gpa <<endl;
+    cout<< cgpa <<endl;
+    cout<< isMale <<endl;
+
+    cout<< "abc" <<endl;
+    cout<< 'A' <<endl;
+    cout<< false <<endl;
+    cout<< 54 <<endl;
+    cout<< 4.5 <<endl;
+
+    return 0;
+}
