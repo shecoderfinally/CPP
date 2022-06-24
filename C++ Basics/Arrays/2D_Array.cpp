93dbbc2ad1413307864204a60605ead1f1533fb4
shecoderfinally <53052899+shecoderfinally@users.noreply.github.com>
2022-06-24 10:03:55 +0530

    updating the content

--- C++ Basics/Arrays/2D_Array.cpp
+++ C++ Basics/Arrays/2D_Array.cpp
@@ -10,6 +10,17 @@ int main()
     cout << numberGrid[0][1] << endl;
     // To access element '5'
     cout << numberGrid[2][0] << endl;
+
+    //Accessing all the elements
+    cout<< "Accessing all the elements: " << endl;
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            cout << numberGrid[i][j] << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
