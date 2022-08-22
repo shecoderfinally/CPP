162944c7659f95cad557489ba893c5c005f43bcb
shecoderfinally <53052899+shecoderfinally@users.noreply.github.com>
2022-08-22 21:50:07 +0530

    Create Sum_of_5_numbers.cpp

---
+++ Practice/Sum_of_5_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    clrscr();
+    float a,b,c,d,e,sum,avg;
+    cout<<"Enter the 5 numbers: \n";
+    cin>>a>>b>>c>>d>>e;
+    sum=(a+b+c+d+e+f);
+    avg=sum/5;
+    cout<<"\n sum:"<<sum;
+    cout<<"\n avg:"<<avg;
+    getch();
+    return 0;
+}
